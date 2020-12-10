3cdd5a35cf97d01020db1feb7eeabb295f5f3b4a
Sophia Dooley <sophiadooley865@gmail.com>
2020-12-09 20:28:59 -0600

    part one done

---
+++ db/index.js
@@ -0,0 +1,43 @@
+const my_password = process.env.PASSWORD;
+
+// inside db/index.js
+const { Client } = require("pg"); // imports the pg module
+
+// supply the db name and location of the database
+const client = new Client(
+  `postgres://postgres:${my_password}@localhost/juiceboxdev`
+);
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+      FROM users;
+    `
+  );
+
+  return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const { rows } = await client.query(
+      `
+          INSERT INTO users(username, password) 
+          VALUES($1, $2) 
+          ON CONFLICT (username) DO NOTHING 
+          RETURNING *;
+        `,
+      [username, password]
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// and export them
+module.exports = {
+  client,
+  getAllUsers,
+  createUser,
+};
